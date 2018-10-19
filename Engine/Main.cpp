e6db63e1eb6d6f91c4452f3acc7765b66d90065d
MARK LUTTRELL <marktylerluttrell@gmail.com>
2018-10-18 22:18:15 -0500

    MarkMAsterCommit

--- Engine/Main.cpp
+++ Engine/Main.cpp
@@ -74,6 +74,6 @@ int WINAPI wWinMain( HINSTANCE hInst,HINSTANCE,LPWSTR pArgs,INT )
 		MessageBox( nullptr,L"\n\nException caught at main window creation.",
 			L"Unhandled Non-STL Exception",MB_OK );
 	}
-
+	//MasterMark
 	return 0;
 }
